86c733db9d530f7dab945305e6bb70ff1a2dddc6
d-korolev <korolev.denize@gmail.com>
2015-02-22 09:39:00 -0800

    Added FFTW

---
+++ MusicalTuner/FFTW/FFTWrapper.cpp
@@ -0,0 +1,104 @@
+﻿#include "FFTWrapper.h"
+#include <string.h>
+#include <math.h>
+#include <float.h>
+
+using namespace FFTW;
+using namespace Platform;
+
+#define mag(x,y) sqrtf((x)*(x) + (y)*(y))
+#define min(x,y) ((x) > (y) ? (y) : (x))
+
+FFTWrapper::FFTWrapper(unsigned int N)
+{
+	this->N = N;
+	this->inputBuffer = (float*) fftwf_malloc(sizeof(float)*N);
+	this->outputBuffer = (fftwf_complex *) fftwf_malloc(sizeof(fftwf_complex)*(N/2 + 1));
+
+	this->plan = fftwf_plan_dft_r2c_1d( N, inputBuffer, outputBuffer, FFTW_MEASURE );
+	this->iplan = fftwf_plan_dft_c2r_1d( N, outputBuffer, inputBuffer, FFTW_MEASURE );
+}
+
+FFTWrapper::~FFTWrapper() {
+	fftwf_destroy_plan( this->plan );
+	fftwf_destroy_plan( this->iplan );
+	fftwf_free( this->outputBuffer );
+	fftwf_free( this->inputBuffer );
+}
+
+unsigned int FFTWrapper::getLength() {
+	return this->N;
+}
+
+Platform::Array<Complex>^ FFTWrapper::fft(const Platform::Array<float>^ input) {
+	// First, we gotta copy input into inputBuffer.  We only copy as much as we can,
+	// that is, min(N, input->Length).  If input->Length < N, then we zero-pad up to N
+	memcpy( this->inputBuffer, input->Data, sizeof(float)*min(this->N, input->Length) );
+
+	// Zero-pad
+	if( this->N > input->Length )
+		memset( this->inputBuffer + input->Length*sizeof(float), 0, sizeof(float)*(this->N - input->Length) );
+
+	// Make fftw do its magic
+	fftwf_execute( this->plan );
+
+	// The resultant spectrum is in outputBuffer!
+	Platform::Array<Complex>^ outputArray = ref new Platform::Array<Complex>((Complex *)this->outputBuffer, (this->N/2 + 1) );
+	return outputArray;
+}
+
+Platform::Array<float>^ FFTWrapper::ifft(const Platform::Array<Complex>^ input) {
+	// First, we copy from input into outputBuffer
+	memcpy( this->outputBuffer, input->Data, sizeof(Complex)*min(this->N/2 + 1, input->Length) );
+
+	// Zero-pad, if we need to (this is weird, but let's be consistent)
+	if( this->N/2 +1 > input->Length )
+		memset( this->outputBuffer + input->Length*sizeof(Complex), 0, sizeof(Complex)*(this->N/2 + 1 - input->Length) );
+
+	// Make fftw do its magic
+	fftwf_execute( this->iplan );
+
+	// Undo FFT scaling on inputBuffer
+	for( unsigned int i=0; i<this->N; ++i )
+		this->inputBuffer[i] /= this->N;
+
+	// The resultant spectrum is in inputBuffer!
+	Platform::Array<float>^ outputArray = ref new Platform::Array<float>((float *)this->inputBuffer, this->N );
+	return outputArray;
+}
+
+
+Platform::Array<float>^ FFTWrapper::fftMag(const Platform::Array<float>^ input) {
+	// First, we gotta copy input into inputBuffer.  :(
+	memcpy( this->inputBuffer, input->Data, sizeof(float)*min(this->N,input->Length) );
+
+	// Zero-pad
+	if( this->N > input->Length )
+		memset( this->inputBuffer + input->Length*sizeof(float), 0, sizeof(float)*(this->N - input->Length) );
+
+	// Make fftw do its magic
+	fftwf_execute( this->plan );
+
+	// Allocate a chunk of memory N/2 + 1 samples long
+	Platform::Array<float>^ outputArray = ref new Platform::Array<float>(this->N/2 + 1);
+
+	// Calculate the magnitude of each FFT bin
+	float * outputArrayData = outputArray->begin();
+	for( unsigned int i=0; i<this->N/2 + 1; ++i ) {
+		outputArrayData[i] = mag(outputBuffer[i][0], outputBuffer[i][1]);
+	}
+
+	return outputArray;
+}
+
+Platform::Array<float>^ FFTWrapper::fftLogMag(const Platform::Array<float>^ input) {
+	// Make fftMag do most of the work for us,
+	Platform::Array<float>^ data = fftMag( input );
+
+	// Take the log of every element:
+	float * dataPtr = data->begin();
+	for( unsigned int i=0; i<data->Length; ++i )
+		dataPtr[i] = logf(dataPtr[i]);
+
+	return data;
+}
---
+++ MusicalTuner/FFTW/FFTWrapper.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "include/fftw3.h"
+
+namespace FFTW
+{
+	// This is a simple value struct that packs memory the same way as an fftwf_complex
+	public value struct Complex {
+		float real, imag;
+	};
+
+
+
+
+    public ref class FFTWrapper sealed
+    {
+	private:
+		// Internally managed buffers
+		float * inputBuffer;
+		fftwf_complex * outputBuffer;
+
+		// Length of buffers
+		unsigned int N;
+
+		// The fftw plan we generate in the constructor, and the inverse plan
+		fftwf_plan plan;
+		fftwf_plan iplan;
+
+    public:
+        FFTWrapper( unsigned int N );
+		virtual ~FFTWrapper();
+
+		// Returns the length this FFTW calculates for us
+		unsigned int getLength();
+
+		// Calculate the complex-valued FFT, returning it as an array.  Note that this FFT will now zero-pad for you!
+		Platform::Array<Complex>^ fft( const Platform::Array<float>^ input );
+
+		// Calculate the inverse FFT, returning it as a real array.  Note that this method expects (N/2 + 1) samples!
+		Platform::Array<float>^ ifft( const Platform::Array<Complex>^ input );
+
+		// Returns the magnitude-only FFT.  Note that this FFT will now zero-pad for you!
+		Platform::Array<float>^ fftMag( const Platform::Array<float>^ input );
+
+		// Outputs the log-magnitude of the fft, same logical length as fft(), but doesn't have two elements per bin
+		// Note that this FFT will now zero-pad for you!
+		Platform::Array<float>^ fftLogMag( const Platform::Array<float>^ input );
+    };
+}
