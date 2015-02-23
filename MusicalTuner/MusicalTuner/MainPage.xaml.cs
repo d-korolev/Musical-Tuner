d7a2d1aa495b8cb64db75fe643b9dfd2fb84ed4f
sbbassam <sbbassam@hotmail.com>
2015-02-22 19:14:38 -0800

    Sara - Last Changes

--- MusicalTuner/MusicalTuner/MainPage.xaml.cs
+++ MusicalTuner/MusicalTuner/MainPage.xaml.cs
@@ -12,7 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using libsound;
+using libfilter;
+using FFTW;
 
 
 namespace MusicalTuner
@@ -23,6 +25,19 @@ namespace MusicalTuner
 
     public sealed partial class MainPage : Page
     {
+        private SoundIO sio;
+        private AudioTool at;
+
+
+        private FFTWrapper fft;
+
+        // This is our matched filter that we will use to search for the initialization sequence
+        private Filter matchedFilt;
+
+        private bool recording = false;
+        private int samples_to_wait = 0;
+        private float[] buffer = new float[10 * 480];
+        private int bufferIdx = 0;
 
         public MainPage()
         {
@@ -34,17 +49,89 @@ namespace MusicalTuner
       
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            
+            // Create sound input
+            sio = new SoundIO();
+            sio.start();
+
+            // Create our audio tool so that we can generate the synchronization code in the appropriate format
+            at = new AudioTool(sio.getInputNumChannels(), sio.getInputSampleRate());
+
+            sio.audioInEvent += (float[] data) =>
+            {
+                process_audio(data);
+            };
+
+
+
         }
 
 
-
-
-
-        private void TuneDropList(object sender, DragEventArgs e)
+        // Here we start recording, so we wait for 4800 more samples to pass, then check FFTs
+        private void start_recording(int idx)
         {
+            samples_to_wait = 3 * 480 - idx;
+            recording = true;
+        }
+
+        private void process_audio(float[] data)
+        {
+            if (!recording)
+                return;
+
+            // Wait until samples_to_wait is zero, then start feeding samples into our buffer
+            if (samples_to_wait >= data.Length)
+            {
+                // If this is the case, we're not getting anything from this buffer
+                samples_to_wait -= data.Length;
+                return;
+            }
+
+            // Otherwise, fill up buffer until it's full
+            int i = 0;
+            while (bufferIdx < buffer.Length)
+            {
+                buffer[bufferIdx] = data[i + samples_to_wait];
+                bufferIdx++;
+                i++;
+                if (i + samples_to_wait >= data.Length)
+                {
+                    samples_to_wait = 0;
+                    return;
+                }
+            }
+
+            // Now that we have waited long enough, let's take the FFT of the next buffer, display it to the user, and decide if it's a one or a zero
+            float[] BUFFER = fft.fftMag(buffer);
+            bufferIdx = 0;
+            recording = false;
+
+            uint N = Convert.ToUInt32(data.Length);
+            float sampleRate = sio.getInputSampleRate();
+
+            //// Detect Freq.
+            float maxValue = BUFFER.Max();
+            float maxIndex = BUFFER.ToList().IndexOf(maxValue);
+            float detectedFrequency = maxIndex * (sampleRate / N);
+
+            Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                this.pitchOut.Text = "Output Frequency: " + (detectedFrequency / 1000.0).ToString("#0.##") + " KHz";
+            });
+
 
         }
+
+        private int getMaxIdx(float[] input)
+        {
+            int maxIdx = 0;
+            for (int i = 1; i < input.Length; ++i)
+            {
+                if (Math.Abs(input[i]) > Math.Abs(input[maxIdx]))
+                    maxIdx = i;
+            }
+            return maxIdx;
+        }
+
 
         private void GuiterTunesCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
