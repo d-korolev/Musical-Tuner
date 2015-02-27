ff6dba493edab9e369b8d6d5e63ad756d2bfcd08
sbbassam <sbbassam@yahoo.com>
2015-02-26 18:47:58 -0800

    Sara - FFT Method

--- MusicalTuner/MusicalTuner/MainPage.xaml.cs
+++ MusicalTuner/MusicalTuner/MainPage.xaml.cs
@@ -24,13 +24,8 @@ namespace MusicalTuner
     public sealed partial class MainPage : Page
     {
         private SoundIO sio;
-        private AudioTool at;
-
 
         private FFTWrapper fft;
-
-        // This is our matched filter that we will use to search for the initialization sequence
-        private Filter matchedFilt;
 
         private bool recording = false;
         private int samples_to_wait = 0;
@@ -50,24 +45,20 @@ namespace MusicalTuner
             // Create sound input
             sio = new SoundIO();
             sio.start();
+            sio.audioInEvent += sio_audioInEvent;
+        }
 
-            // Create our audio tool so that we can generate the synchronization code in the appropriate format
-            at = new AudioTool(sio.getInputNumChannels(), sio.getInputSampleRate());
-
-            sio.audioInEvent += (float[] data) =>
-            {
-                process_audio(data);
-            };
-
-
-
+        void sio_audioInEvent(float[] data)
+        {
+            recording = true;
+            process_audio(data);
         }
 
 
         // Here we start recording, so we wait for 4800 more samples to pass, then check FFTs
         private void start_recording(int idx)
         {
-            samples_to_wait = 3 * 480 - idx;
+            samples_to_wait = 480 - idx;
             recording = true;
         }
 
@@ -76,46 +67,20 @@ namespace MusicalTuner
             if (!recording)
                 return;
 
-            // Wait until samples_to_wait is zero, then start feeding samples into our buffer
-            if (samples_to_wait >= data.Length)
-            {
-                // If this is the case, we're not getting anything from this buffer
-                samples_to_wait -= data.Length;
-                return;
-            }
-
-            // Otherwise, fill up buffer until it's full
-            int i = 0;
-            while (bufferIdx < buffer.Length)
-            {
-                buffer[bufferIdx] = data[i + samples_to_wait];
-                bufferIdx++;
-                i++;
-                if (i + samples_to_wait >= data.Length)
-                {
-                    samples_to_wait = 0;
-                    return;
-                }
-            }
-
-            // Now that we have waited long enough, let's take the FFT of the next buffer, display it to the user, and decide if it's a one or a zero
-            float[] BUFFER = fft.fftMag(buffer);
-            bufferIdx = 0;
-            recording = false;
-
             uint N = Convert.ToUInt32(data.Length);
             float sampleRate = sio.getInputSampleRate();
 
+            fft = new FFTWrapper(N);
+            float[] fftmag = fft.fftMag(data);
             //// Detect Freq.
-            float maxValue = BUFFER.Max();
-            float maxIndex = BUFFER.ToList().IndexOf(maxValue);
+            float maxValue = fftmag.Max();
+            float maxIndex = fftmag.ToList().IndexOf(maxValue);
             float detectedFrequency = maxIndex * (sampleRate / N);
-
             Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                this.pitchOut.Text = "Output Frequency: " + (detectedFrequency / 1000.0).ToString("#0.##") + " KHz";
+                this.pitchOut.Text = "Output Frequency: " + (detectedFrequency ).ToString("#0.##") + " KHz";
+                this.octaveOut.Text = "Mag: " + maxValue;
             });
-
 
         }
 
